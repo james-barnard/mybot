3060eaa5197d47b060c46ca34ba446b95704f1a3
shoaib <hayajia@gmail.com>
2016-12-06 23:46:56 +0500

    Bot work * Receive Message * Save user information * Check if location is present * Respond back the user

--- app/webhooks/message_received_callback.rb
+++ app/webhooks/message_received_callback.rb
@@ -9,6 +9,27 @@ class MessageReceivedCallback < MessageQuickly::Callback
   end
 
   def run
+    coordinates = ''
+    if json['entry'].first['messaging'].first['message']['attachments'].present?
+      coordinates = json['entry'].first['messaging'].first['message']['attachments'].first['payload']['coordinates'].inspect
+  	end
+    user_id = json['entry'].first['messaging'].first['sender']['id']
+    user_info = MessageQuickly::Api::UserProfile.find(user_id)
+
+    puts "=========="
+    puts user_info.inspect
+    puts "=========="
+    
+    recipient = MessageQuickly::Messaging::Recipient.new(id: user_id)
+
+    delivery = MessageQuickly::Api::Messages.create(recipient) do |message|
+      if coordinates == ''
+        message.text = 'Welcome, Please Share Your Location'
+      else
+        message.text = 'We Have Got Your Location, Thank You very Much'
+      end
+    end
+    return {user_info: user_info, user_id: user_id, coordinates: coordinates}
   end
 
 end
